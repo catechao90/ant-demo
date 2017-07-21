84b4c96fcb736c839c2b2728841688bb270a27b1
cate.zhao <zhao.jing@wowoohr.com>
2017-07-21 11:44:24 +0800

    added react-router-dom

---
+++ src/components/Index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import './Index.css';
+const { Header, Content, Footer } = Layout;
+
+
+class Index extends React.Component {  
+  render() {
+    return (
+      <div>index</div>
+    );
+  }
+}
+
+
+export default Index;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Layout, Menu, Breadcrumb, Row, Col } from 'antd';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import '@/index.css';
+import App from '@/components/App';
+import Index from '@/components/Index';
 import registerServiceWorker from './registerServiceWorker';
-const { Header, Content, Footer } = Layout;
-
+import { Layout, Menu, Breadcrumb } from 'antd'
+const { Header, Content, Footer } = Layout
 
 ReactDOM.render(
-	<Layout id='app'>
-	    <Header style={{ position: 'fixed', width: '100%' }}>
-	      <div className="logo" />
-	      <Menu
-	        theme="dark"
-	        mode="horizontal"
-	        defaultSelectedKeys={['2']}
-	        style={{ lineHeight: '64px' }}
-	      >
-	        <Menu.Item key="1">nav 1</Menu.Item>
-	        <Menu.Item key="2">nav 2</Menu.Item>
-	        <Menu.Item key="3">nav 3</Menu.Item>
-	      </Menu>
-	    </Header>
-	    <Content style={{ padding: '0 50px', marginTop: 64 }}>
-			<Breadcrumb style={{ margin: '12px 0' }}>
-				<Breadcrumb.Item>Home</Breadcrumb.Item>
-				<Breadcrumb.Item>List</Breadcrumb.Item>
-				<Breadcrumb.Item>App</Breadcrumb.Item>
-			</Breadcrumb>
-	      	<div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
-				<Row>
-					<Col span={8}><App /></Col>
-					<Col span={8}>col-8</Col>
-					<Col span={8}>col-8</Col>
-			    </Row>
-			</div>
-	      
-	    </Content>
-	    <Footer style={{ textAlign: 'center' }}>
-	      Ant Design ©2016 Created by Ant UED
-	    </Footer>
-  	</Layout>, document.getElementById('root'));
+	<Router>
+		<div>
+			<Layout className="layout">
+		        <Header>
+		        <div className="logo" />
+		        <Menu
+		          theme="dark"
+		          mode="horizontal"
+		          defaultSelectedKeys={['2']}
+		          style={{ lineHeight: '64px' }}
+		        >
+		          <Menu.Item key="1">nav 1</Menu.Item>
+		          <Menu.Item key="2">nav 2</Menu.Item>
+		          <Menu.Item key="3">nav 3</Menu.Item>
+		        </Menu>
+		        </Header>
+		        <Content style={{ padding: '0 50px' }}>
+		          <Breadcrumb style={{ margin: '12px 0' }}>
+		            <Breadcrumb.Item>Home</Breadcrumb.Item>
+		            <Breadcrumb.Item>List</Breadcrumb.Item>
+		            <Breadcrumb.Item>App</Breadcrumb.Item>
+		          </Breadcrumb>
+		          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+		          	<Link to="/index">index</Link>
+		          	<Route path="/index" component={Index}/>
+		          </div>
+		        </Content>
+		        <Footer style={{ textAlign: 'center' }}>
+		        Ant Design ©2016 Created by Ant UED
+		        </Footer>
+	      	</Layout>			
+		</div>
+  	</Router>, document.getElementById('root'));
 registerServiceWorker();
